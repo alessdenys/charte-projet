b85c4a39327068d2c36db7321d48cf4b13fda952
alessdenys <alessandro.denys1999@gmail.com>
2019-12-12 16:39:24 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const { watch, src, dest } = require("gulp");
+const tailwindConfig = "tailwind.config.js"; /* Path to Tailwind config */
+const mainCSS = "src/style.css"; /* Path to main stylesheet */
+const mainHTML = "index.html"
+/**
+ * Custom PurgeCSS Extractor
+ * https://github.com/FullHuman/purgecss
+ */
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g);
+  }
+}
+
+const css = () => {
+  const atimport = require("postcss-import");
+  const postcss = require("gulp-postcss");
+  const tailwindcss = require("tailwindcss");
+  const purgecss = require("gulp-purgecss");
+
+  return src(mainCSS)
+    .pipe(postcss([atimport(), tailwindcss(tailwindConfig)]))
+    .pipe(
+      purgecss({
+        content: ["**/*.html"],
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ["css"]
+          }
+        ]
+      })
+    )
+    .pipe(dest("dist/"));
+};
+
+exports.css = css;
+exports.default = () => {
+  watch(mainHTML, css);
+};
